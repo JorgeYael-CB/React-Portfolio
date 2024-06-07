54f907e0ed59f34ce7e4467a7e588c97078143a0
JorgeYael-CB <jorge.yael333@gmail.com>
2024-06-06 20:53:51 -0600

    Agregamos la paginacion y sistema de mostrar preguntas

--- src/presentation/components/questions/QuestionApp.tsx
+++ src/presentation/components/questions/QuestionApp.tsx
@@ -1,6 +1,23 @@
+import { QuestionApiInterface } from "../../../interfaces";
 
-export const QuestionApp = () => {
+export const QuestionApp = ({ date, question, title, user }: QuestionApiInterface) => {
+  const formattedDate = new Date(date).toLocaleDateString();
+
   return (
-    <div>QuestionApp</div>
-  )
-}
+    <div className="max-w-2xl my-4 mx-auto p-6 bg-white rounded-xl shadow-md">
+      <div className="flex justify-between items-center mb-4">
+        <div>
+          <p className="text-lg font-medium text-gray-700">{user.name}</p>
+          <p className="text-sm text-gray-500">{formattedDate}</p>
+        </div>
+        <div className={`text-sm font-medium ${user.verify ? 'text-green-500' : 'text-red-500'}`}>
+          {user.verify ? 'Verified User' : 'User Not Verified'}
+        </div>
+      </div>
+      <div>
+        <h2 className="text-2xl font-semibold text-gray-800 mb-2">{title}</h2>
+        <p className="text-gray-700">{question}</p>
+      </div>
+    </div>
+  );
+};
