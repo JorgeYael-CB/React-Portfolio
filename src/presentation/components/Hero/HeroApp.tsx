cc3a8f76909b7b415204c6a7cec3412e4e440fba
JorgeYael-CB <jorge.yael333@gmail.com>
2024-06-09 12:13:04 -0600

    Animaciones agregadas

--- src/presentation/components/Hero/HeroApp.tsx
+++ src/presentation/components/Hero/HeroApp.tsx
@@ -1,8 +1,11 @@
+import 'animate.css';
+
+
 
 export const HeroApp = () => {
   return (
     <section className="bg-white dark:bg-gray-900 p-8">
-        <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+        <div className="animate__animated animate__fadeInLeft py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12 animate__fadeInDown">
             <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">We invest in the world’s potential</h1>
             <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Here at DevComplete Studios we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
             <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
--- src/presentation/components/about-me/SkillsApp.tsx
+++ src/presentation/components/about-me/SkillsApp.tsx
@@ -1,13 +1,29 @@
+import { useEffect, useState } from "react";
 import { SkillApp } from "./SkillApp";
+import { useInView } from 'react-intersection-observer';
+import 'animate.css';
 
 export const SkillsApp = () => {
   const skills = ['css-3', 'git', 'github', 'html-5', 'javascript', 'typescript', 'lua', 'mongo', 'nest', 'node', 'postgres', 'postman', 'python', 'react', 'sql', 'tailwind'];
+  const [isView, setIsView] = useState(false);
+  const { ref, inView } = useInView({
+    threshold: 0,
+  });
+
+  useEffect(() => {
+    if (inView) {
+      setIsView(true);
+    }
+  }, [inView]);
 
   return (
     <div className="mt-12 mb-36 py-6 px-4 shadow-sm bg-gray-100 rounded-sm" id="skills">
       <h3 className="text-center mb-6 text-black md:text-4xl text-3xl font-bold md:my-4 my-12">Skills</h3>
 
-      <div className="flex flex-wrap justify-center sm:justify-start gap-4">
+      <div
+        ref={ ref }
+        className={`flex flex-wrap justify-center sm:justify-start gap-4 ${isView ? 'animate__animated animate__fadeInRight' : ''}`}
+      >
         {skills.map(skill => (
           <SkillApp key={skill} skill={skill} />
         ))}
