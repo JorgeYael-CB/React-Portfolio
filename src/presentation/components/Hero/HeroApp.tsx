cb0e2fef7148b48c5e35e9fe5e3dd1c8da9459c2
JorgeYael-CB <jorge.yael333@gmail.com>
2024-06-09 17:23:04 -0600

    Hero actualizado diseño

--- src/presentation/components/Hero/HeroApp.tsx
+++ src/presentation/components/Hero/HeroApp.tsx
@@ -1,37 +1,28 @@
 import 'animate.css';
-
-
+import Typewriter from 'typewriter-effect';
 
 export const HeroApp = () => {
   return (
-    <section className="bg-white dark:bg-gray-900 p-8">
-        <div className="animate__animated animate__fadeInLeft py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12 animate__fadeInDown">
-            <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">We invest in the world’s potential</h1>
-            <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Here at DevComplete Studios we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
-            <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
-                <a href="#about-me" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
-                    Learn more
-                    <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-                </a>
-            </div>
-            <div className="px-4 mx-auto text-center md:max-w-screen-md lg:max-w-screen-lg lg:px-36">
-                <span className="font-semibold text-gray-400 uppercase">FEATURED IN</span>
-                <div className="flex flex-wrap justify-center items-center mt-8 text-gray-500 sm:justify-between">
-                    <a href="https://www.youtube.com/channel/UCaorjdhDs4JMW1QvwmKJv-g" target="_blank" className="mr-5 mb-5 lg:mb-0 hover:text-gray-800 dark:hover:text-gray-400">
-                        <img src="/icons/YT.svg" alt="Icon YT" className="h-16" />
-                        <p className="mt-4 text-center text-white font-medium text-xl">Youtube</p>
-                    </a>
-                    <a target="_blank" href="https://github.com/JorgeYael-CB" className="mr-5 mb-5 lg:mb-0 hover:text-gray-800 dark:hover:text-gray-400">
-                        <img src="/icons/github_blanco.svg" alt="Icon github" className="h-16" />
-                        <p className="mt-4 text-center text-white font-medium text-xl">Github</p>
-                    </a>
-                    <a target="_blank" href="https://www.linkedin.com/in/jorge-yael-chino-5a5037306/" className="mr-5 mb-5 lg:mb-0 hover:text-gray-800 dark:hover:text-gray-400">
-                        <img className="h-16" src="/icons/linkedin.svg" alt="Icon linkedin" />
-                        <p className="mt-2 text-center text-white font-medium text-xl">Linkedin</p>
-                    </a>
-                </div>
-            </div>
+    <section className='h-dvh bg-black relative'>
+      <div className='text-white text-center py-4 relative z-10 animate__animated animate__fadeInLeft'>
+        <h1 className='md:text-6xl text-5xl font-bold pt-40'>Backend Web Developer</h1>
+        <p className='md:text-xl text-lg font-normal mt-3 text-white'>Jorge Yael CB</p>
+        <div id='typewriter' className='md:text-lg text-base mt-5 italic'>
+          <Typewriter
+            options={{
+              strings: ['Transforming ideas into scalable solutions', 'JavaScript Enthusiast'],
+              autoStart: true,
+              loop: true,
+            }}
+          />
         </div>
+      </div>
+
+      <a href="#about-me" className='absolute bottom-36 left-1/2 transform -translate-x-1/2 text-blue-500 animate__animated animate__fadeIn'>
+        <svg className='w-8 h-8 animate-bounce' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
+          <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M19 9l-7 7-7-7'></path>
+        </svg>
+      </a>
     </section>
-  )
-}
+  );
+};
--- src/presentation/components/navBar/NavBarApp.tsx
+++ src/presentation/components/navBar/NavBarApp.tsx
@@ -1,18 +1,21 @@
+import 'animate.css';
+
+
 interface Props {
   title?: string;
 }
 
 export const NavBarApp = ({ title = 'DevComplete Studios' }: Props) => {
   return (
-    <div className="max-w-full mx-auto py-4 px-8 bg-gray-700">
-      <div className="flex flex-col md:flex-row md:justify-between md:gap-0 gap-y-6 items-center">
-        <h1 className="text-2xl font-semibold text-white text-center md:text-left mb-4 md:mb-0">
-          {title}
+    <div className="max-w-full mx-auto py-4 px-8 bg-black">
+      <div className="flex flex-col md:flex-row md:justify-between md:gap-0 gap-y-6 items-center animate__animated animate__fadeIn">
+        <h1 className="text-2xl font-semibold text-blue-500 text-center md:text-left mb-4 md:mb-0">
+          { title }
         </h1>
-        <nav className="flex flex-col md:flex-row md:gap-3 gap-6">
-          <a className="text-gray-200 uppercase text-sm font-semibold hover:text-white transition-all" href="#contact">Contact</a>
-          <a className="text-gray-200 uppercase text-sm font-semibold hover:text-white transition-all" href="#questions">Questions</a>
-          <a className="text-gray-200 uppercase text-sm font-semibold hover:text-white transition-all" href="#skills">Skills</a>
+        <nav className="flex flex-col md:flex-row md:gap-3 gap-6 text-center">
+          <a className="text-blue-500 uppercase text-base font-semibold hover:underline" href="#contact">Contact</a>
+          <a className="text-blue-500 uppercase text-base font-semibold hover:underline" href="#questions">Questions</a>
+          <a className="text-blue-500 uppercase text-base font-semibold hover:underline" href="#skills">Skills</a>
         </nav>
       </div>
     </div>
